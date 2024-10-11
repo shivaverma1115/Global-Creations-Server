3a3ed8ef38c4a6febe3d177ada79c0b47ca3c875
shivainnovatrix <sverma@innovatrixinfotech.com>
2024-10-11 11:01:03 +0530

    /api added to all routes

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,6 @@ const port = process.env.PORT || 5000;
 
 require("dotenv").config();
 // conncet with mongodb atlas
-// const mongoUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qbmtaop.mongodb.net/orgado`;
 const mongoUrl = `${process.env.MONGODB_URL}`;
 const mongooseOptions: any = {
   useNewUrlParser: true,
@@ -15,7 +14,7 @@ async function mongodbConnect() {
   try {
     await mongoose.connect(mongoUrl, mongooseOptions);
     console.log("databes connected");
-    app.get("/", (req, res) => {
+    app.get("/api", (req, res) => {
       res.send("Website is running");
     });
     app.listen(port, () => {
